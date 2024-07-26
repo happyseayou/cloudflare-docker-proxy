94e784fdd745e23300e3a8d0ae188e9476c5278f
Yangfan Xie <40071609+happyseayou@users.noreply.github.com>
2024-07-26 10:20:22 +0000

    update

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.bughub.work": dockerHub,
+  "quay.bughub.work": "https://quay.io",
+  "gcr.bughub.work": "https://gcr.io",
+  "k8s-gcr.bughub.work": "https://k8s.gcr.io",
+  "k8s.bughub.work": "https://registry.k8s.io",
+  "ghcr.bughub.work": "https://ghcr.io",
+  "cloudsmith.bughub.work": "https://docker.cloudsmith.io",
+  "ecr.bughub.work": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.bughub.work": dockerHub,
 };
 
 function routeByHosts(host) {
